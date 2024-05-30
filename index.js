bea867ae61531ee1da43b4250f1bb722eca76646
Denys Paziak <67861412+Denys-Paziak@users.noreply.github.com>
2024-05-30 15:14:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ connection.connect((err) => {
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/img', express.static(path.join(__dirname, 'images')));
 
 // Routes
 app.get('/', (req, res) => {
