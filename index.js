f4b4b5753dbf707f39f6dbf805e9c375ef4d0255
Denys Paziak <67861412+Denys-Paziak@users.noreply.github.com>
2024-06-17 11:55:49 +0300

    fix

--- index.js
+++ index.js
@@ -30,7 +30,23 @@ app.use(express.json());
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 
-app.use(cors({ origin: 'http://localhost:3000' }));
+
+const allowedOrigins = ['http://localhost:3000', 'https://prostopoo.vercel.app', 'http://127.0.0.1:5500'];
+
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions));
 
 
 const dbConfig = {
