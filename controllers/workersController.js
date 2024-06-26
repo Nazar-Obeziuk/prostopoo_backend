1ebc0d68eb96266ba927a3ff73afe35c064127e2
Denys Paziak <67861412+Denys-Paziak@users.noreply.github.com>
2024-06-26 13:22:27 +0300

    CRUD Workers

--- controllers/workersController.js
+++ controllers/workersController.js
@@ -1,4 +1,3 @@
-
 const mysql = require('mysql');
 const dbConfig = require('../config/dbConfig');
 
@@ -16,6 +15,96 @@ exports.getWorkers = (req, res) => {
                 return res.status(500).send('Server error');
             }
             res.json(results);
+            connection.end();
+        });
+    });
+};
+
+exports.getWorker = (req, res) => {
+    const connection = mysql.createConnection(dbConfig);
+    const { id } = req.params;
+    connection.connect(err => {
+        if (err) {
+            console.error('Error connecting to database: ' + err.stack);
+            return res.status(500).send('Database connection error');
+        }
+        const sqlQuery = 'SELECT * FROM workers WHERE id = ?';
+        connection.query(sqlQuery, [id], (err, results) => {
+            if (err) {
+                console.error('Error executing query:', err.message);
+                return res.status(500).send('Server error');
+            }
+            if (results.length === 0) {
+                return res.status(404).send('Worker not found');
+            }
+            res.json(results[0]);
+            connection.end();
+        });
+    });
+};
+
+exports.createWorker = (req, res) => {
+    const connection = mysql.createConnection(dbConfig);
+    const { name_ua, name_en, subtitle_ua, subtitle_en, first_description_ua, first_description_en, second_description_ua, second_description_en, third_description_ua, third_description_en } = req.body;
+
+    connection.connect(err => {
+        if (err) {
+            console.error('Error connecting to database: ' + err.stack);
+            return res.status(500).send('Database connection error');
+        }
+
+        const sqlQuery = 'INSERT INTO workers (name_ua, name_en, subtitle_ua, subtitle_en, first_description_ua, first_description_en, second_description_ua, second_description_en, third_description_ua, third_description_en) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+        connection.query(sqlQuery, [name_ua, name_en, subtitle_ua, subtitle_en, first_description_ua, first_description_en, second_description_ua, second_description_en, third_description_ua, third_description_en], (err, results) => {
+            if (err) {
+                console.error('Error executing query:', err.message);
+                return res.status(500).send('Server error');
+            }
+            res.status(201).json({ message: 'Worker created successfully', workerId: results.insertId });
+            connection.end();
+        });
+    });
+};
+
+exports.updateWorker = (req, res) => {
+    const connection = mysql.createConnection(dbConfig);
+    const { id } = req.params;
+    const { name_ua, name_en, subtitle_ua, subtitle_en, first_description_ua, first_description_en, second_description_ua, second_description_en, third_description_ua, third_description_en } = req.body;
+
+    connection.connect(err => {
+        if (err) {
+            console.error('Error connecting to database: ' + err.stack);
+            return res.status(500).send('Database connection error');
+        }
+
+        const sqlQuery = 'UPDATE workers SET name_ua = ?, name_en = ?, subtitle_ua = ?, subtitle_en = ?, first_description_ua = ?, first_description_en = ?, second_description_ua = ?, second_description_en = ?, third_description_ua = ?, third_description_en = ? WHERE id = ?';
+        connection.query(sqlQuery, [name_ua, name_en, subtitle_ua, subtitle_en, first_description_ua, first_description_en, second_description_ua, second_description_en, third_description_ua, third_description_en, id], (err, results) => {
+            if (err) {
+                console.error('Error executing query:', err.message);
+                return res.status(500).send('Server error');
+            }
+            res.json({ message: 'Worker updated successfully' });
+            connection.end();
+        });
+    });
+};
+
+exports.deleteWorker = (req, res) => {
+    const connection = mysql.createConnection(dbConfig);
+    const { id } = req.params;
+
+    connection.connect(err => {
+        if (err) {
+            console.error('Error connecting to database: ' + err.stack);
+            return res.status(500).send('Database connection error');
+        }
+
+        const sqlQuery = 'DELETE FROM workers WHERE id = ?';
+        connection.query(sqlQuery, [id], (err, results) => {
+            if (err) {
+                console.error('Error executing query:', err.message);
+                return res.status(500).send('Server error');
+            }
+            res.json({ message: 'Worker deleted successfully' });
             connection.end();
         });
     });
--- routes/workersRoutes.js
+++ routes/workersRoutes.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const workersController = require('../controllers/workersController');
+const { authenticateToken, authorizeAdmin } = require('../middleware/authMiddleware');
 
 router.get('/', workersController.getWorkers);
+router.get('/:id', workersController.getWorker);
+
+router.post('/', authenticateToken, authorizeAdmin, workersController.createWorker);
+router.put('/:id', authenticateToken, authorizeAdmin, workersController.updateWorker);
+router.delete('/:id', authenticateToken, authorizeAdmin, workersController.deleteWorker);
 
 module.exports = router;
