ff6d832f053384a200315b479022635bf2573814
Denys Paziak <67861412+Denys-Paziak@users.noreply.github.com>
2024-06-29 16:43:40 +0300

    get to post auth login

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,58 +1,15 @@
 const express = require('express');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const mysql = require('mysql');
-const dbConfig = require('../config/dbConfig');
-const multer = require('multer'); // Додайте цей рядок
-
 const router = express.Router();
-const upload = multer(); // Додайте цей рядок
+const authController = require('../controllers/authController');
+const { authenticateToken, authorizeAdmin } = require('../middleware/authMiddleware');
 
-router.post('/register', upload.none(), async (req, res) => {
-    const connection = mysql.createConnection(dbConfig);
-    const { username, email, password, role } = req.body;
-    const userRole = role || 'user';
+router.post('/login', authController.login);
+router.post('/register', authController.register);
 
-    try {
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const query = 'INSERT INTO users (username, email, password, role) VALUES (?, ?, ?, ?)';
 
-        connection.query(query, [username, email, hashedPassword, userRole], (err, results) => {
-            if (err) {
-                return res.status(500).json({ error: err.message });
-            }
-            res.status(201).json({ message: 'User registered successfully!' });
-        });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+router.post('/admin', authenticateToken, authorizeAdmin, (req, res) => {
+    res.json({ message: 'Welcome to the admin panel!' });
 });
 
-router.post('/login', upload.none(), (req, res) => {
-    const connection = mysql.createConnection(dbConfig);
-    const { email, password } = req.body;
-
-    const query = 'SELECT * FROM users WHERE email = ?';
-
-    connection.query(query, [email], async (err, results) => {
-        if (err) {
-            return res.status(500).json({ error: err.message });
-        }
-
-        if (results.length === 0) {
-            return res.status(401).json({ message: 'Invalid email or password' });
-        }
-
-        const user = results[0];
-        const isMatch = await bcrypt.compare(password, user.password);
-
-        if (!isMatch) {
-            return res.status(401).json({ message: 'Invalid email or password' });
-        }
-
-        const token = jwt.sign({ id: user.id, role: user.role }, 'your_jwt_secret', { expiresIn: '30d' });
-        res.json({ message: 'Login successful', token });
-    });
-});
 
 module.exports = router;
