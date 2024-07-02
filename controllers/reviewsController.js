9356251cbcb225c06788fdc6f0e66e2071c3661f
Denys Paziak <67861412+Denys-Paziak@users.noreply.github.com>
2024-07-02 23:15:32 +0300

    rev

--- controllers/reviewsController.js
+++ controllers/reviewsController.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 const dbConfig = require('../config/dbConfig');
 
-exports.getReviews = (req, res) => {
+exports.getGeneralReviews = (req, res) => {
     const connection = mysql.createConnection(dbConfig);
 
     connection.connect((err) => {
@@ -11,7 +11,7 @@ exports.getReviews = (req, res) => {
             return;
         }
 
-        const sqlQuery = 'SELECT * FROM reviews';
+        const sqlQuery = 'SELECT * FROM reviews WHERE product_id IS NULL';
         connection.query(sqlQuery, (err, results) => {
             if (err) {
                 console.error('Error executing query:', err.message);
@@ -24,32 +24,32 @@ exports.getReviews = (req, res) => {
     });
 };
 
-exports.getReview = (req, res) => {
+exports.getReviewsByProductId = (req, res) => {
     const connection = mysql.createConnection(dbConfig);
-    const { id } = req.params;
+    const { product_id } = req.params;
 
     connection.connect((err) => {
         if (err) {
             console.error('Error connecting to database: ' + err.stack);
-            return res.status(500).send('Database connection error');
+            res.status(500).send('Database connection error');
+            return;
         }
 
-        const sqlQuery = 'SELECT * FROM reviews WHERE id = ?';
-        connection.query(sqlQuery, [id], (err, results) => {
+        const sqlQuery = 'SELECT * FROM reviews WHERE product_id = ?';
+        connection.query(sqlQuery, [product_id], (err, results) => {
             if (err) {
                 console.error('Error executing query:', err.message);
-                return res.status(500).send('Server error');
+                res.status(500).send('Server error');
+            } else {
+                res.json(results);
             }
-            if (results.length === 0) {
-                return res.status(404).send('No entry found with the given ID');
-            }
-            res.json(results[0]);
             connection.end();
         });
     });
 };
 
-exports.createReview = (req, res) => {
+
+exports.createGeneralReview = (req, res) => {
     const connection = mysql.createConnection(dbConfig);
 
     connection.connect((err) => {
@@ -60,8 +60,6 @@ exports.createReview = (req, res) => {
         }
 
         const { stars, name_ua, name_en, description_ua, description_en, pluses_ua, pluses_en, minuses_ua, minuses_en } = req.body;
-
-        console.log('Received data:', req.body);
 
         const sqlQuery = `
             INSERT INTO reviews (stars, name_ua, name_en, description_ua, description_en, pluses_ua, pluses_en, minuses_ua, minuses_en) 
@@ -80,9 +78,9 @@ exports.createReview = (req, res) => {
     });
 };
 
-exports.updateReview = (req, res) => {
+exports.createProductReview = (req, res) => {
     const connection = mysql.createConnection(dbConfig);
-    const { id } = req.params;
+    const { product_id } = req.params;
     const { stars, name_ua, name_en, description_ua, description_en, pluses_ua, pluses_en, minuses_ua, minuses_en } = req.body;
 
     connection.connect((err) => {
@@ -92,48 +90,17 @@ exports.updateReview = (req, res) => {
             return;
         }
 
-        console.log('Received data for update:', req.body);
-
         const sqlQuery = `
-            UPDATE reviews 
-            SET stars = ?, name_ua = ?, name_en = ?, description_ua = ?, description_en = ?, pluses_ua = ?, pluses_en = ?, minuses_ua = ?, minuses_en = ?
-            WHERE id = ?
+            INSERT INTO reviews (product_id, stars, name_ua, name_en, description_ua, description_en, pluses_ua, pluses_en, minuses_ua, minuses_en) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
         `;
 
-        connection.query(sqlQuery, [stars, name_ua, name_en, description_ua, description_en, pluses_ua, pluses_en, minuses_ua, minuses_en, id], (err, results) => {
+        connection.query(sqlQuery, [product_id, stars, name_ua, name_en, description_ua, description_en, pluses_ua, pluses_en, minuses_ua, minuses_en], (err, results) => {
             if (err) {
                 console.error('Error executing query:', err.message);
                 res.status(500).send('Server error');
-            } else if (results.affectedRows === 0) {
-                res.status(404).send('No entry found with the given ID');
             } else {
-                res.status(200).send(`Entry with ID ${id} updated successfully`);
-            }
-            connection.end();
-        });
-    });
-};
-
-exports.deleteReview = (req, res) => {
-    const connection = mysql.createConnection(dbConfig);
-
-    connection.connect((err) => {
-        if (err) {
-            console.error('Error connecting to database: ' + err.stack);
-            res.status(500).send('Database connection error');
-            return;
-        }
-
-        const { id } = req.params;
-        const sqlQuery = 'DELETE FROM reviews WHERE id = ?';
-        connection.query(sqlQuery, [id], (err, results) => {
-            if (err) {
-                console.error('Error executing query:', err.message);
-                res.status(500).send('Server error');
-            } else if (results.affectedRows === 0) {
-                res.status(404).send('No entry found with the given ID');
-            } else {
-                res.status(200).send(`Entry with ID ${id} deleted successfully`);
+                res.status(201).send({ id: results.insertId, product_id, stars, name_ua, name_en, description_ua, description_en, pluses_ua, pluses_en, minuses_ua, minuses_en });
             }
             connection.end();
         });
--- routes/reviewsRoutes.js
+++ routes/reviewsRoutes.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const reviewsController = require('../controllers/reviewsController');
-const { authenticateToken, authorizeAdmin } = require('../middleware/authMiddleware');
 
-router.get('/', reviewsController.getReviews);
-router.get('/:id', reviewsController.getReview);
+router.get('/general', reviewsController.getGeneralReviews);
+router.post('/general', reviewsController.createGeneralReview);
 
-router.post('/', authenticateToken, authorizeAdmin, reviewsController.createReview);
-router.put('/:id', authenticateToken, authorizeAdmin, reviewsController.updateReview);
-router.delete('/:id', authenticateToken, authorizeAdmin, reviewsController.deleteReview);
+router.get('/product/:product_id', reviewsController.getReviewsByProductId);
+router.post('/product/:product_id', reviewsController.createProductReview);
+
 
 module.exports = router;
