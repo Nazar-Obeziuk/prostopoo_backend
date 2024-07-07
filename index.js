8c43f60e0f6083b51eac8c5e58c610cdf9766f3d
Nazar Obeziuk <nazarobeziuk@MacBook-Pro-Nazar.local>
2024-07-07 10:29:38 +0200

    sdfg

--- index.js
+++ index.js
@@ -1,51 +1,44 @@
 // up
 
 // Lib
-const express = require('express');
-const cors = require('cors');
-const mysql = require('mysql');
-const path = require('path');
-const bodyParser = require('body-parser');
-const multer = require('multer');
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql");
+const path = require("path");
+const bodyParser = require("body-parser");
+const multer = require("multer");
 
 // Config
 const app = express();
-const PORT = process.env.PORT || 4001;
-
+const PORT = process.env.PORT || 4002;
 
 app.use(cors());
 const upload = multer();
 
-app.use(bodyParser.json());
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-
-app.get('/', (req, res) => {
-    res.send('Welcome to PROSTOPOO API');
+app.get("/", (req, res) => {
+  res.send("Welcome to PROSTOPOO API");
 });
 
 // Use
 app.use(express.json());
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 // Routes
-const productsRoutes = require('./routes/productsRoutes');
-const authRoutes = require('./routes/authRoutes');
-const workersRoutes = require('./routes/workersRoutes');
-const reviewsRoutes = require('./routes/reviewsRoutes');
-const storageRoutes = require('./routes/storageRoutes');
-const variationRoutes = require('./routes/variationsRoutes');
+const productsRoutes = require("./routes/productsRoutes");
+const authRoutes = require("./routes/authRoutes");
+const workersRoutes = require("./routes/workersRoutes");
+const reviewsRoutes = require("./routes/reviewsRoutes");
+const storageRoutes = require("./routes/storageRoutes");
+const variationRoutes = require("./routes/variationsRoutes");
 
-app.use('/products', productsRoutes);
-app.use('/auth', authRoutes);
-app.use('/workers', workersRoutes);
-app.use('/reviews', reviewsRoutes);
-app.use('/variations', variationRoutes);
+app.use("/products", productsRoutes);
+app.use("/auth", authRoutes);
+app.use("/workers", workersRoutes);
+app.use("/reviews", reviewsRoutes);
+app.use("/variations", variationRoutes);
 
-app.use('/storage', storageRoutes);
-
+app.use("/storage", storageRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
